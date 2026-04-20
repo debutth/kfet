7ff72f3c4673b626ea882816bd25103e447879fa
debutth <thomas.debut@outlook.fr>
2026-04-20 22:42:07 +0200

    firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,39 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
 
-// ⚠️ REMPLACE CES VALEURS PAR LES TIENNES DE FIREBASE
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-    apiKey: "TON_API_KEY",
-    authDomain: "bde-polytech-marseille.firebaseapp.com",
-    projectId: "bde-polytech-marseille",
-    storageBucket: "bde-polytech-marseille.appspot.com",
-    messagingSenderId: "TON_MESSAGE_SENDER_ID",
-    appId: "TON_APP_ID"
+
+  apiKey: "AIzaSyC-ZsriKsw7DbtrOkgV5ByEcb4FdZiP4CQ",
+
+  authDomain: "kfet-stje.firebaseapp.com",
+
+  projectId: "kfet-stje",
+
+  storageBucket: "kfet-stje.firebasestorage.app",
+
+  messagingSenderId: "812434401817",
+
+  appId: "1:812434401817:web:9321d2ea1e4934a0e91888",
+
+  measurementId: "G-KF6MDWVYKM"
+
 };
 
+
+// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+
+const analytics = getAnalytics(app);
